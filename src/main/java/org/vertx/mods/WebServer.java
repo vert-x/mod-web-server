c0cf79c2139c5f87af11bb5bdc3d12caac84ae66
purplefox <timvolpe@gmail.com>
2012-07-21 08:40:01 +0100

    removed netty dependency

--- src/main/java/org/vertx/mods/WebServer.java
+++ src/main/java/org/vertx/mods/WebServer.java
@@ -16,7 +16,6 @@
 
 package org.vertx.mods;
 
-import org.jboss.netty.handler.codec.http.HttpHeaders;
 import org.vertx.java.busmods.BusModBase;
 import org.vertx.java.core.Handler;
 import org.vertx.java.core.file.impl.PathAdjuster;
@@ -81,8 +80,8 @@ public class WebServer extends BusModBase implements Handler<HttpServerRequest> 
 
   public void handle(HttpServerRequest req) {
     // browser gzip capability check
-    String acceptEncoding = req.headers().get(HttpHeaders.Names.ACCEPT_ENCODING);
-    boolean acceptEncodingGzip = acceptEncoding == null ? false : acceptEncoding.contains(HttpHeaders.Values.GZIP);
+    String acceptEncoding = req.headers().get("accept-encoding");
+    boolean acceptEncodingGzip = acceptEncoding == null ? false : acceptEncoding.contains("gzip");
 
     String fileName = webRootPrefix + req.path;
     if (req.path.equals("/")) {
@@ -93,7 +92,7 @@ public class WebServer extends BusModBase implements Handler<HttpServerRequest> 
         File file = new File(PathAdjuster.adjust(fileName + ".gz"));
         if (file.exists()) {
           // found file with gz extension
-          req.response.putHeader(HttpHeaders.Names.CONTENT_ENCODING, HttpHeaders.Values.GZIP);
+          req.response.putHeader("content-encoding", "gzip");
           req.response.sendFile(fileName + ".gz");
         } else {
           // not found gz file, try to send uncompressed file
